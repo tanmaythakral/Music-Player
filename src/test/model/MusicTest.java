b7f94e3003b3806af026a71653c60ceb29f7ce92
tanmay thakral <46583379+Kingshadow285@users.noreply.github.com>
2021-02-15 00:11:04 +0530

    minor changes

--- src/test/model/MusicTest.java
+++ src/test/model/MusicTest.java
@@ -2,7 +2,7 @@ package model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
+
 
 
 import static org.junit.jupiter.api.Assertions.*;
